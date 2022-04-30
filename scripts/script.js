de6dfbb521cdb2d44e235eb8bed17cb305c1289a
ernest-lamptey <ernest.lamptey@amalitech.org>
2022-04-30 17:38:20 +0000

    Display operation choice

--- scripts/script.js
+++ scripts/script.js
@@ -2,28 +2,36 @@ const firstNumberSlot = document.querySelector('#first_number');
 const secondNumberSlot = document.querySelector('#second_number');
 const operationSlot = document.querySelector('#operation');
 const resultSlot = document.querySelector('#result');
+const addBtn = document.querySelector('#addition')
+const subtractBtn = document.querySelector('#subtraction')
+const multiplyBtn = document.querySelector('#multiplication')
+
+
+//Event listener for class 'operations'
+const operationsArr = [addBtn, subtractBtn, multiplyBtn]
+operationsArr.forEach(item => {
+    item.addEventListener('click', () => {
+        operationSlot.innerHTML = item.innerHTML
+    })
+})
 
 function generateRandomNumber(max){
     return Math.round(Math.random() * (max - 1)) + 1;
 }
 
 function getResult(){
-    // 1 ---> +
-    // 2 ---> -
-    // 3 ---> *
-    // 4 ---> /
     operationID = generateRandomNumber(3)
     switch (operationID) {
-        case 1: 
-        result = firstNumber + secondNumber;
-        break;
-        case 2: 
-        result = firstNumber - secondNumber;
-        break;
-        case 3: 
-        result = firstNumber * secondNumber;
-        break;
-        // case 4: 
+        case 1: //addition
+            result = firstNumber + secondNumber;
+            break;
+        case 2: //subtraction
+            result = firstNumber - secondNumber;
+            break;
+        case 3: //multiplication
+            result = firstNumber * secondNumber;
+            break;
+        // case 4: //division
         // result = firstNumber / secondNumber;
         // break;
     }
@@ -31,10 +39,12 @@ function getResult(){
     return result
 }
 
+// Gameplay
 firstNumber = generateRandomNumber(10);
 secondNumber = generateRandomNumber(10);
 result = getResult()
 
+//update HTML
 firstNumberSlot.textContent = firstNumber;
 secondNumberSlot.textContent = secondNumber;
 resultSlot.textContent = result;
